efa2b4222aed73cd2472feed2c57a9a2016691a6
nadav-swimm <97392592+nadav-swimm@users.noreply.github.com>
2023-01-29 14:04:43 +0900

    Update template.js

--- examples/vanilla-es6/src/template.js
+++ examples/vanilla-es6/src/template.js
@@ -18,13 +18,13 @@ export default class Template {
 	 */
 	itemList(items) {
 		return items.reduce((a, item) => a + `
-<li data-id="${item.id}"${item.completed ? ' class="completed"' : ''}>
-	<div class="view">
+<div data-id="${item.id}"${item.completed ? ' class="completed"' : ''}>
+	<div class="view view-template">
 		<input class="toggle" type="checkbox" ${item.completed ? 'checked' : ''}>
 		<label>${escapeForHTML(item.title)}</label>
 		<button class="destroy"></button>
 	</div>
-</li>`, '');
+</div>`, '');
 	}
 
 	/**
